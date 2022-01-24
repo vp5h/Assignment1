d8fa463d9f79460e267fd17d59c2c603986b967a
Pravesh Vyas <praveshvyas.18@gmail.com>
2022-01-24 11:07:52 +0530

    ChipDone

--- src/Chips/ChipWithInput.js
+++ src/Chips/ChipWithInput.js
@@ -4,11 +4,13 @@ import { ImCross } from "react-icons/im";
 
 export default function ChipsWithInput() {
   let [ChipArr, setChipArr] = useState(["Lemon", "cinamon"]);
+  let [inputVal, setInputVal] = useState("");
 
   function KeyHandler(e) {
     if (e.key === "Enter") {
       if (e.target.value.trim().length > 0)
         setChipArr([...ChipArr, e.target.value]);
+      setInputVal("");
     }
   }
 
@@ -20,33 +22,38 @@ export default function ChipsWithInput() {
   return (
     <>
       <div className="Chip_Component">
-        <div className="allChips">
-          {ChipArr.map((chip) => {
-            return (
-              <>
-                <span className="chip" key={chip}>
-                  {chip}
-                  <span
-                    key={chip}
-                    className="chipIcon"
-                    onClick={() => RemoveChip(chip)}
-                  >
-                    <ImCross key={chip} />
+        <label for="Chip-Input" className="chips">
+          <div className="allChips">
+            {ChipArr.map((chip) => {
+              return (
+                <>
+                  <span className="chip" key={chip}>
+                    {chip}
+                    <span
+                      key={chip}
+                      className="chipIcon"
+                      onClick={() => RemoveChip(chip)}
+                    >
+                      <ImCross key={chip} />
+                    </span>
                   </span>
-                </span>
-              </>
-            );
-          })}
-        </div>
-
-        <input
-          className="Chip-Input"
-          type="text"
-          placeholder="Anything"
-          onKeyUp={(e) => {
-            KeyHandler(e);
-          }}
-        />
+                </>
+              );
+            })}
+          </div>
+          <input
+            className="Chip-Input"
+            type="text"
+            placeholder="Anything"
+            value={inputVal}
+            onChange={(e) => {
+              setInputVal(e.target.value);
+            }}
+            onKeyUp={(e) => {
+              KeyHandler(e);
+            }}
+          />
+        </label>
       </div>
     </>
   );
